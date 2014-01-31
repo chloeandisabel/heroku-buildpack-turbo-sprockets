0561bac5c6b01420d84137a1a6ab1520cc41a79e
David Guthu <davidguthu@gmail.com>
2014-01-31 17:27:04 -0500

    fixed merge bug

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -98,12 +98,17 @@ private
               end
             end
           else
+            log "assets_precompile", :status => "failure"
+            puts "Precompiling assets failed, enabling runtime asset compilation"
+            install_plugins
+            puts "Please see this article for troubleshooting help:"
+            puts "http://devcenter.heroku.com/articles/rails31_heroku_cedar#troubleshooting"
             cache.clear "public/assets"
           end
         else
           log "assets_precompile", :status => "failure"
           puts "Precompiling assets failed, enabling runtime asset compilation"
-          install_plugin("rails31_enable_runtime_asset_compilation")
+          install_plugins
           puts "Please see this article for troubleshooting help:"
           puts "http://devcenter.heroku.com/articles/rails31_heroku_cedar#troubleshooting"
         end
